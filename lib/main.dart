5efe557f6c22748ad16f37540f59e8bddde4c89b
x0vedant <114058796+x0vedant@users.noreply.github.com>
2022-11-01 11:49:34 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,242 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Todo());
+}
+
+class Todo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  // app layout
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('To-do List'),
+        ),
+      )
+      
+    );
+  }
+class _TodoListState extends State<TodoList> {
+  // save data
+  final List<String> _todoList = <String>[];
+  // text field
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // app layout
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-do List'),
+      ),
+    );
+  }
+}
+class _TodoListState extends State<TodoList> {
+  // save data
+  final List<String> _todoList = <String>[];
+  // text field
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // app layout
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-do List'),
+      ),
+    );
+  }
+}
+  class _TodoListState extends State<TodoList> {
+  // save data
+  final List<String> _todoList = <String>[];
+  // text field
+  final TextEditingController _textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // code that returns the appbar
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('To-do List'),
+      ),
+    );
+  }
+
+  // adds data to list.
+  void _addTodoItem(String title) {
+    //  a set state will notify the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+}
+   // adds data to List.
+  void _addTodoItem(String title) {
+    //  a set state will notify the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    // the text field is cleared once the item is added to list
+    _textFieldController.clear();
+  }
+
+   // populate the listview
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+    Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  // display a dialog for the user to enter items
+  Future<AlertDialog> _displayDialog(BuildContext context) async {
+    // alter the app state to show a dialog
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              // add button
+              FlatButton(
+                child: const Text('ADD'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              // cancel button
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+  // iterates through our todo list titles.
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
+  class _TodoListState extends State<TodoList> {
+  final List<String> _todoList = <String>[];
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // app layout
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      // add items to the to-do list
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
+    import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Todo());
+}
+
+class Todo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  // save data
+  final List<String> _todoList = <String>[];
+  // text field
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: ListView(children: _getItems()),
+      // add items to the to-do list
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          child: Icon(Icons.add)),
+    );
+  }
+
+  void _addTodoItem(String title) {
+    // Wrapping it inside a set state will notify
+    // the app that the state has changed
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  // this Generate list of item widgets
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  // display a dialog for the user to enter items
+  Future<AlertDialog> _displayDialog(BuildContext context) async {
+    // alter the app state to show a dialog
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add a task to your list'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: const InputDecoration(hintText: 'Enter task here'),
+          ),
+          actions: <Widget>[
+            // add button
+            FlatButton(
+              child: const Text('ADD'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _addTodoItem(_textFieldController.text);
+              },
+            ),
+            // Cancel button
+            FlatButton(
+              child: const Text('CANCEL'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      } 
+    );
+  }
+  // iterates through our todo list title
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
+}
